2f756d3d70422336ced609fa2064ea8906ccedcf
Denis Artemjev <artemjev@ut.ee>
2025-11-17 00:23:52 +0200

    d

---
+++ script.js
@@ -0,0 +1,9 @@
+// Кнопка Programmeerimine I
+document.getElementById("Programmeerimine_I").addEventListener("click", function() {
+    window.location.href = "progaI.html";
+});
+
+// Кнопка Programmeerimine II
+document.getElementById("Programmeerimine_II").addEventListener("click", function() {
+    window.location.href = "progaII.html";
+});
